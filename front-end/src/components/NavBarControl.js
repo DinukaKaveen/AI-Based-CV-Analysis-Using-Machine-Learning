89e6c347616ebb6a86ca54fda67441a07450392a
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-09-19 19:45:22 +0530

    Update NavBarControl.js

--- front-end/src/components/NavBarControl.js
+++ front-end/src/components/NavBarControl.js
@@ -5,7 +5,7 @@ import NavBar from "./NavBar";
 function NavBarControl() {
   const location = useLocation();
 
-  if (location.pathname === "/regsiter") {
+  if (location.pathname === "/home") {
     return <NavBar />;
   }
 
