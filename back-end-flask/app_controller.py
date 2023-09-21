9b7148e0fce30041573373ecf4a346b8c85510c1
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-09-21 17:50:37 +0530

    update cors

--- back-end-flask/app_controller.py
+++ back-end-flask/app_controller.py
@@ -1,16 +1,24 @@
 from flask import Flask, request, jsonify, session, make_response
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import secrets
 import user_controller
 
 app = Flask(__name__)
 
+# Block all other origins by setting a default CORS configuration
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 # Configure CORS for specific URLs
 CORS(app, resources={
-    r"/login": {"origins": ["http://localhost:3000"]}
+    r"/register": {"origins": ["http://localhost:3000"]}
 })
 
 app.secret_key = secrets.token_hex(16)
+
+# @app.route("/*")
+# @cross_origin()
+# def all_other_routes():
+#     return "This route is blocked."
 
 @app.route('/register', methods=['POST'])
 def register():
