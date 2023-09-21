4375e414836bcde15a9dc158fd41348c514e2674
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-09-21 18:05:03 +0530

    Update app_controller.py

--- back-end-flask/app_controller.py
+++ back-end-flask/app_controller.py
@@ -31,23 +31,5 @@ def login():
     return user_controller.login()
 
 
-@app.route('/set_user', methods=['POST'])
-def set_session_user():
-    user = request.get_json()
-    session['user_id'] = user['user_id']
-    # Set the session cookie in the browser
-    response = make_response(jsonify({'message': 'User ID set in session'}))
-    response.set_cookie('session', session.sid)
-    return response
-
-@app.route('/get', methods=['GET'])
-def get_session():
-    if 'user_id' in session:
-        return jsonify({'message': session['user_id']}) 
-    
-    return jsonify({'message': "no session"} ) 
-
-    #return jsonify({"value": session.get("user_id")}) 
-
 if __name__ == '__main__':
     app.run(host='localhost', port=5000) 
