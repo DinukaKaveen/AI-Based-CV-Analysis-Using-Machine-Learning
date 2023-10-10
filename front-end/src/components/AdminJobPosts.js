05da681f37a930a91368ef23ae02b009d92a3ca8
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-10-10 16:20:52 +0530

    Update AdminJobPosts.js

--- front-end/src/components/AdminJobPosts.js
+++ front-end/src/components/AdminJobPosts.js
@@ -2,7 +2,65 @@ import React from 'react'
 
 function AdminJobPosts() {
   return (
-    <div>AdminJobPosts</div>
+    <div>
+        <div className="p-4 sm:ml-64">
+        <div className="p-4 mt-14">
+          <nav className="flex mb-4" aria-label="Breadcrumb">
+            <ol className="inline-flex items-center space-x-1 md:space-x-3">
+              <li className="inline-flex items-center">
+                <a
+                  href="/admin/home"
+                  className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray"
+                >
+                  <svg
+                    className="w-4 h-4 mr-2"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+                  </svg>
+                  Dashboard
+                </a>
+              </li>
+              <li aria-current="page">
+                <div className="flex items-center">
+                  <svg
+                    className="w-6 h-6 text-gray-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
+                    Job Posts
+                  </span>
+                </div>
+              </li>
+            </ol>
+          </nav>
+          <h2 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl dark:text-gray-700">
+            Job Posts
+          </h2>
+          <br />
+
+          <a
+            href="/admin/create_job_post"
+            type="button"
+            className="text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-100 mr-2 mb-2 border border-gray-200 dark:border-gray-400"
+          >
+            <i className="fa-solid fa-plus"></i>&nbsp;Create New Job
+          </a>
+
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
