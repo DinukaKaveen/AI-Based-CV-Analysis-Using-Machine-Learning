2736cfb8037f846568de59b992f7704c75c91c34
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-10-11 22:47:24 +0530

    Update AdminCreateJobPost.js

--- front-end/src/components/AdminCreateJobPost.js
+++ front-end/src/components/AdminCreateJobPost.js
@@ -59,6 +59,29 @@ function AdminCreateJobPost() {
                   Dashboard
                 </a>
               </li>
+              <li>
+                <div className="flex items-center">
+                  <svg
+                    aria-hidden="true"
+                    className="w-6 h-6 text-gray-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <a
+                    href="/admin/job_posts"
+                    className="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-gray"
+                  >
+                    Job Posts
+                  </a>
+                </div>
+              </li>
               <li aria-current="page">
                 <div className="flex items-center">
                   <svg
